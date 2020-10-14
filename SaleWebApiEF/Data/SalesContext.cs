ac38eb8463eb4c75059858f86f2b506ba46ca70b
Manami Crowley <manami.faye@gmail.com>
2020-10-14 09:55:22 -0400

    Add project files.

---
+++ SaleWebApiEF/Data/SalesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaleWebApiEF.Models;
+
+namespace SaleWebApiEF.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext (DbContextOptions<SalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SaleWebApiEF.Models.Customer> Customer { get; set; }
+    }
+}
